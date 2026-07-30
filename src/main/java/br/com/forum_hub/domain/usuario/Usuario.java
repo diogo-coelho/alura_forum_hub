7b831fc0bb4e2e6c4a1fb2053dae9d6697f78404
Diogo Coelho <diogooliveiracoelho@gmail.com>
2026-07-30 13:52:37 -0300

    Inclusão do pacote de usuaros

---
+++ src/main/java/br/com/forum_hub/domain/usuario/Usuario.java
@@ -0,0 +1,62 @@
+package br.com.forum_hub.domain.usuario;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private Long id;
+
+    private String nomeCompleto;
+
+    private String nomeUsuario;
+
+    private String biografia;
+
+    private String miniBiografia;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String senha;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public String getBiografia() {
+        return biografia;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public String getMiniBiografia() {
+        return miniBiografia;
+    }
+
+}
